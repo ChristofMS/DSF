973851215e62c48d13ada9c65a2913ed8ce14041
Lukas Eichelberger <master@vpn-unisg-130-82-69-118.unisg.ch>
2018-11-13 14:26:51 +0100

    Update Test.R

--- Test.R
+++ Test.R
@@ -6,18 +6,3 @@ rm(list = ls())
 
 
 flkv
-
-
-
-hjlfbukr
-baanaaa
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-banana
-=======
-
->>>>>>> fb2bfe0ecacd7e227f8fe6a4b9d4c92b6a149842
-=======
-hallo
->>>>>>> 0a566fb48b93f52e9dfe8792bdae62fd69b24d1c
